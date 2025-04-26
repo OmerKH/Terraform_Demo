49bd4108ac219afa011045da92ac2d923ca46e07
Omer Kirshner <omer.khs@gmail.com>
2025-04-26 17:54:58 +0300

    add var

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
-provider "aws" {}
-
-variable cidr_blocks {
-  description = "cidr blocks for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  })) 
+provider "aws" {
+  region = "us-east-1"
 }
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable env_zone {}
+variable avail_zone{}
+
+# variable cidr_blocks {
+#   description = "cidr blocks for vpc and subnets"
+#   type = list(object({
+#     cidr_block = string
+#     name = string
+#   })) 
+# }
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.cidr_blocks[0].cidr_block
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_zone}-vpc"
   }
 }
 
@@ -20,6 +27,6 @@ resource "aws_subnet" "myapp-subnet-1" {
   cidr_block = var.cidr_blocks[1].cidr_block
   availability_zone = "us-east-1a"
   tags = {
-    Name: var.cidr_blocks[1].name
+    Name: "${var.env_zone}-subnet-1"
   }
 }
